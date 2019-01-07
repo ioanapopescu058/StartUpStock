046fd1d3d705f31de227ac0db13c815549b87c28
Gianina <gianina.m.carp@gmail.com>
2019-01-07 07:12:22 +0200

    for final

---
+++ menu.js
@@ -0,0 +1,29 @@
+<nav class="colorlib-nav" role="navigation">
+    <div class="top-menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <div id="colorlib-logo"><a href="index.html">Start Up Stock</a></div>
+                </div>
+                <div class="col-md-10 text-right menu-1">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li class="has-dropdown">
+                            <a class="active">Fiki</a>
+                            <ul class="dropdown">
+                                <li><a href="m1.html">M1</a></li>
+                                <li><a href="m2.html">M2</a></li>
+                                <li><a href="m3.html">M3</a></li>
+                                <li><a href="m4+5.html">M4+5</a></li>
+                                <li><a href="m6.html">M6</a></li>
+                                <li><a href="m7.html">M7</a></li>
+                                <li><a href="m8.html">M8</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="about.html">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
